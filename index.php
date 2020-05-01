696f428ba4fa450c6d2a9eda2383e4dc3e1c89ab
Assis Ferreira <ferreira.assisruny@gmail.com>
2020-05-01 11:09:39 -0100

    Start page

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Julia | Chatbot Engine</title>
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+	<main class="container py-4 text-center">
+		
+		<br>
+		<br>
+		<h1>Julia | Chatbot Engine</h1>
+
+		<br>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit ullam neque sint eaque sequi ad molestias, voluptate commodi cupiditate doloremque consequuntur beatae perferendis nostrum non nihil aut excepturi. Ratione, impedit.</p>
+		
+		<br>
+		<div>
+			<a href="#">Home</a> |
+			<a href="#">Demo</a> |
+			<a href="#">Doc</a>
+		</div>
+		
+	</main>
+	
+</body>
+</html>
